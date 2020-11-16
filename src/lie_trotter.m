b1eb38bc3ca26258558c0f86a6a9af0cd88187bf
nachito <igvidaurreta@gmail.com>
2020-11-16 18:32:54 -0300

    feat: add Lie Trotter algorithm

---
+++ src/lie_trotter.m
@@ -0,0 +1,21 @@
+function U = LieTrotter(U, k, delta_t, order, is_reverse)
+    for n=1:order
+        if is_reverse % Reverse step we apply (Non Linear o Linear).
+            U = nonlinear(U, k, delta_t/order );
+            U = linear(U, k, delta_t/order);
+        else % On foward step (Linear o Non Linear)
+            U = linear(U, k, delta_t/order);
+            U = nonlinear(U, k, delta_t/order);
+        end
+    end
+end
+
+% Linear step of Lie Trotter
+function U = linear(U, k, delta_t)
+    U = U.*exp(1i*k.^3*delta_t);
+end
+
+% Non linear step of Lie Trotter algorithm
+function U = nonlinear(U, k, delta_t)
+    U = U - (3i*k*delta_t).*fft((real(ifft(U))).^2);
+end
