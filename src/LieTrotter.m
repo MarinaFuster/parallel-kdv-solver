8181905e389049b2af3df8027adb3445c1d39214
marina <mfuster@itba.edu.ar>
2020-11-18 07:44:40 -0300

    Filename must be the same as function

--- src/LieTrotter.m
+++ src/LieTrotter.m
@@ -1,9 +1,9 @@
 function U = LieTrotter(U, k, delta_t, order, is_reverse)
     for n=1:order
-        if is_reverse % Reverse step we apply (Non Linear o Linear).
+        if is_reverse                           % Reverse step we apply (Non Linear o Linear).
             U = nonlinear(U, k, delta_t/order );
             U = linear(U, k, delta_t/order);
-        else % On foward step (Linear o Non Linear)
+        else                                    % On foward step (Linear o Non Linear)
             U = linear(U, k, delta_t/order);
             U = nonlinear(U, k, delta_t/order);
         end
@@ -12,10 +12,10 @@ end
 
 % Linear step of Lie Trotter
 function U = linear(U, k, delta_t)
-    U = U.*exp(1i*k.^3*delta_t);
+    U = U.*exp(1i*k.^3*delta_t); % From applying Fourier transform to equation
 end
 
 % Non linear step of Lie Trotter algorithm
 function U = nonlinear(U, k, delta_t)
-    U = U - (3i*k*delta_t).*fft((real(ifft(U))).^2);
+    U = U - (3i*k*delta_t).*fft((real(ifft(U))).^2); %From applying Fourier transform to equation
 end
