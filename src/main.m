612261f62c412eed6e5c3e404d0269cf556ffd67
nachito <igvidaurreta@gmail.com>
2020-11-22 17:54:47 -0300

    add input/output flow

--- src/main.m
+++ src/main.m
@@ -1,20 +1,57 @@
 clc
 
-% MODE: normal or metrics
-runMetrics = false;
+continue_asking = true;
 
+while continue_asking
+    run_mode = input('What mode do you want to run? (metrics|normal)> ', 's');
+    if ~strcmp(run_mode, 'metrics') && ~strcmp(run_mode, 'normal')
+        disp('Invalid option, try again :)')
+    else
+        continue_asking = false;
+    end
+end
+if strcmp(run_mode, 'metrics')
+    runMetrics=true;
+else
+    runMetrics = false;
+end
+
+continue_asking = true;
+if ~runMetrics
+    order = input("What order would you like to run the algorihtm?> ");
+    delta_t = input("What deltaT would you like to run it with? (default: 0.0005)> ");
+
+    while continue_asking
+        p_mode = input('Should we run parallel mode or serial?(parallel|serial)> ', "s");
+        if ~strcmp(p_mode, 'parallel')&& ~strcmp(p_mode, 'serial')
+            disp('Inalid option. Mode must be parallel or serial.')
+        else
+            continue_asking = false;
+        end
+    end
+
+    if strcmp(p_mode, 'parallel')
+        parallel = true
+    else
+        parallel = false
+    end
+else
+    % Metrics mode
+    disp("Running in metrics mode with orders 2, 4 and 6")
+    orders = [2, 4, 6];
+    repetitions = 5
+end
 % Normal mode
-order = 4;
-delta_t = 0.0005;
-parallel = false;
+%order = 4;
+%delta_t = 0.0005;
+%parallel = false;
 
-% Metrics mode
-repetitions = 5;
-orders = [2, 4, 6];
+
+
 delta_ts = [0.001, 0.005, 0.0001, 0.0005];
 
 N = 256;
-x = linspace(-10,10,N);        % domain, is this ok?
+x = linspace(-10,10,N);
 tmax =  1.5;
 t=0;
 
@@ -35,7 +72,7 @@ drawnow
 if(runMetrics)
     fileID = fopen('metrics.txt','w');
     fprintf(fileID,'%s\t%s\t%s\t%s\t%s\n','time','order', 'parallel', 'delta_t', 'repetition');
-    
+
     parallel = false;
     for i=1:length(orders)
         for j=1:length(delta_ts)
@@ -58,7 +95,7 @@ if(runMetrics)
                 tic;
                 result = Approximate(u, x, N, tmax, orders(i), parallel, delta_ts(j));
                 t_parallel = toc;
-                
+
                 fprintf(fileID,'%E\t%d\t%s\t%f\t%d\n', t_parallel, orders(i), string(parallel), delta_ts(j), rep);
             end
         end
@@ -67,10 +104,5 @@ if(runMetrics)
     fclose(fileID);
 else
     % Normal mode
-     Approximate(u, x, N, tmax, order, parallel, delta_t);
+    Approximate(u, x, N, tmax, order, parallel, delta_t);
 end
-
-% in main you define the equation you want to aproximate and its initial
-% conditions
-% Here you call Approximate -> MethodStep (for each iteration) ->
-% LieTrotter
